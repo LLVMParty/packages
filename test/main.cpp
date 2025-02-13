bcaa2dad08602b1bd7877287809dc28d411c96cd
Duncan Ogilvie <mr.exodia.tpodt@gmail.com>
2025-02-13 19:49:28 +0100

    Add initial test project

---
+++ test/main.cpp
@@ -0,0 +1,27 @@
+#include <nlohmann/json.hpp>
+#include <toml.hpp>
+#include <httplib.h>
+// gflags
+// glog
+// googletest
+// hiredis
+#include <tsl/ordered_map.h>
+#include <fmt/format.h>
+#include <spdlog/spdlog.h>
+#include <LIEF/LIEF.hpp>
+#include <capstone/capstone.h>
+#include <z3++.h>
+// minisat
+// stp
+// xed
+// gmp
+#include <bitwuzla/cpp/bitwuzla.h>
+#include <llvm/IR/Module.h>
+#include <remill/Arch/Instruction.h>
+// alive2
+// souper
+#include <souper/Inst/Inst.h>
+
+int main() {
+    llvm::LLVMContext C;
+}
